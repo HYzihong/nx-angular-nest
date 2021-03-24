ed6d76f496853f5fe92eb4ee9da75150c0512c78
hy <1300706240@qq.com>
2021-03-24 16:09:53 +0800

    feat(angular): add ng-zorro-antd

--- apps/admin/src/app/app.component.ts
+++ apps/admin/src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'nx-angular-nest-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less'],
+  styleUrls: ['./app.component.less']
 })
 export class AppComponent {
+  isCollapsed = false;
   title = 'admin';
 }
